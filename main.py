54b0df4bbe659b23d2f8dc399259316d4286af0f
sjpokedoke <70572845+sjpokedoke@users.noreply.github.com>
2021-09-26 15:15:15 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+video = cv2.VideoCapture(0)
+image = cv2.imread("picture.png")
+
+while True:
+    ret, frame = video.read()
+   # print(frame)
+
+    frame = cv2.resize(frame, (640,480))
+    image = cv2.resize(frame, (640, 480))
+
+    upperblack = np.array([104, 153, 70])
+    lowerblack = np.array([30, 30, 0])
+
+    mask = cv2.inRange(frame, lowerblack, upperblack)
+    res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    f = frame - res
+    f = np.where(f == 0, image, f)
+    
+    cv2.imshow("video", frame)
+    cv2.imshow("mask", f)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+video.release()
+cv2.destroyAllWindows()
